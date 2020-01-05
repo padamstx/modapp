0379b4f9e661c32a6c6e450fcd609708d77f7b24
Phil Adams <phil_adams@us.ibm.com>
2020-01-05 10:28:38 -0600

    Update lib dependency to v3.0.2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/padamstx/modapp
 
 go 1.12
 
-require github.com/padamstx/gomodlib/v2 v2.1.0
+require github.com/padamstx/gomodlib/v3 v3.0.2
--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	lib "github.com/padamstx/gomodlib/v2"
+	lib "github.com/padamstx/gomodlib/v3"
 )
 
-const version string = "2.0.0"
+const version string = "3.0.0"
 
 func main() {
 	fmt.Printf("modapp main, version %s\n", version)
